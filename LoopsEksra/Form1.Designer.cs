32b5a6ff7b7538a3bf88facab7f4e0a145bd7881
skrrrm <a@macbook-windows>
2020-04-01 09:28:18 +0200

    Add project files.

---
+++ LoopsEksra/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace LoopsEksra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 25;
+            this.listBoxResult.Location = new System.Drawing.Point(68, 77);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(612, 704);
+            this.listBoxResult.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(711, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(436, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Øvelse1: Hel-tal fra 1 - 1000";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(711, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(436, 69);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Øvelse2: Hel-tal fra 1 - 1000";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(711, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(436, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Øvelse3: Stjerner";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(711, 397);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(436, 69);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Øvelse4: Hel-tal fra 100 - 1";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(711, 507);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(436, 69);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Øvelse5: Heltal mellem 30 - 40";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(711, 612);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(436, 69);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Øvelse6: Pi med flere og flere dicimaler";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(711, 712);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(436, 69);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Øvelse7: Juletræ";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1221, 864);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ LoopsEksra/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoopsEksra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Øvelse 1
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                listBoxResult.Items.Add(i); // add i to listbox items
+            }
+        }
+
+        /// <summary>
+        /// Øvelse 2
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i=+2)
+            {
+                listBoxResult.Items.Add(i); // add i to listbox items
+            }
+        }
+
+        /// <summary>
+        /// Øvelse 3
+        /// </summary>
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Øvelse 4
+        /// </summary>
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Øvelse 5
+        /// </summary>
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Øvelse 6
+        /// </summary>
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Øvelse 7
+        /// </summary>
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
